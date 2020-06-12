23692b35d28697f0927859ab6e1b7af824298a22
rajat-jain-au5 <jrajat862@gmail.com>
2020-06-12 16:14:15 +0530

    make it better

---
+++ README.md
@@ -0,0 +1,15 @@
+## API implementation using Nodejs
+
+#### features:
+1. Users can register.
+2. Users can login.
+3. Users can add an article.
+4. Users can view all articles, sort and pagination.
+
+### How to use:-
+  1. first clone the repo
+  2. ### `npm install` 
+  3. ### `npm start`
+
+there are four APIs
+ 
--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const uri =
-  "mongodb+srv://rajat:vishal221198@cluster0-vwqjo.mongodb.net/Api?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(
   async (res) => {
     console.log("mongodb is connected succesfully");
