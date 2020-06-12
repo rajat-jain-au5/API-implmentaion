27b28135d53744fae33e5a12457bc9af1c1df696
rajat-jain-au5 <jrajat862@gmail.com>
2020-06-12 16:18:21 +0530

    make it better

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use("/", articleRouter);
 app.use('/', usersRouter);
 
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/userController");
 
+router.post('/',(req,res)=>{
+   console.log("app is running")
+})
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
